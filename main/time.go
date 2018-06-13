b3f6c59af2474fbd144a4da8d47ac4cc03cf199b
wangcheng <kingcheeng163@gmail.com>
2018-06-14 00:46:33 +0800

    Go的时间包time...

---
+++ main/time.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	fmt.Println(now)
+
+	fmt.Println(now.Year())
+	fmt.Println(now.Month())
+	fmt.Println(now.Weekday())
+	fmt.Println(now.Day())
+	fmt.Println(now.Hour())
+	fmt.Println(now.Minute())
+	fmt.Println(now.Second())
+}
